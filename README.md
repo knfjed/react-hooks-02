ae41e784a96568b8d7bd5d8cfe4830f1ae5f509b
knfjed <knfjed@gmail.com>
2021-01-21 16:45:11 +0900

    複数の状態を一つのオブジェクトに統合

--- README.md
+++ README.md
@@ -12,3 +12,21 @@ git push -u origin main
 ## コマンド
 
 - 削除　 git rm src/App.css
+
+## Spread Attributes
+
+参考:https://yami-beta.hateblo.jp/entry/2016/09/06/112330
+
+子の Component へ props を渡す場合，Spread Attributes を用いると，以下のように書き換えられる。
+
+- 1 つずつ渡す場合
+  `const props = { foo: "foo", bar: "bar" };`
+  `render() {`
+  `return <Child foo={props.foo} bar={props.bar} />`
+  `}`
+
+- Spread Attributes を用いて渡す場合
+  `const props = { foo: "foo", bar: "bar" };`
+  `render() {`
+  `return <Child {...props} />`
+  `}`
--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 
 const App = (props) => {
-  const [name, setName] = useState(props.name);
-  const [price, setPrice] = useState(props.price);
-  const reset = () => {
-    setPrice(props.price);
-    setName(props.name);
-  };
+  // stateにnameとpriceを集約
+  const [state, setState] = useState(props);
+  // 分割代入　state.name、state.priceを省略
+  const { name, price } = state;
 
   return (
     <>
       <p>
         現在の{name}は{price}円です
       </p>
-      {/* イベントを拾って、setNameで変更。e.target.valueはお約束 */}
-      <input value={name} onChange={(e) => setName(e.target.value)}></input>
-      <button onClick={() => setPrice(price + 1)}>+1</button>
-      <button onClick={() => setPrice(price - 1)}>-1</button>
-      <button onClick={reset}>reset</button>
+      <input
+        value={name}
+        onChange={(e) => setState({ ...state, name: e.target.value })}
+      ></input>
+      <button onClick={() => setState({ ...state, price: price + 1 })}>
+        +1
+      </button>
+      <button onClick={() => setState({ ...state, price: price - 1 })}>
+        -1
+      </button>
+      <button onClick={() => setState(props)}>reset</button>
     </>
   );
 };
