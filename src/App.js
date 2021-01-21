4afc5f9a699e0de6f7e9cb77f9a78294b3eaa4c2
knfjed <knfjed@gmail.com>
2021-01-21 15:48:51 +0900

    複数のuseStateを活用

--- src/App.js
+++ src/App.js
@@ -1,52 +1,30 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment2 = () => setCount((previousCount) => previousCount + 1);
-
-  const decrement2 = () => setCount((previousCount) => previousCount - 1);
-
-  const double = () => setCount((previousCount) => previousCount * 2);
-
-  const reset = () => setCount(0);
-
-  const divide3 = () => {
-    setCount((previousCount) =>
-      previousCount % 3 === 0 ? previousCount / 3 : previousCount
-    );
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
   };
-
-  // 別の書き方
-  // const divide3 = () => {
-  //   setCount((previousCount) => {
-  //     if (previousCount % 3 === 0) {
-  //       return previousCount / 3;
-  //     } else {
-  //       return previousCount;
-  //     }
-  //   });
-  // };
 
   return (
     <>
-      <div>cont: {count}</div>
-
-      <div>
-        <button onClick={() => setCount(count + 1)}>+1</button>
-        <button onClick={() => setCount(count - 1)}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-        <button onClick={double}>*2</button>
-        <button onClick={reset}>reset</button>
-      </div>
-      <div>
-        <button onClick={divide3}>3の倍数の時、3倍にする</button>
-      </div>
+      <p>
+        現在の{name}は{price}円です
+      </p>
+      {/* イベントを拾って、setNameで変更。e.target.valueはお約束 */}
+      <input value={name} onChange={(e) => setName(e.target.value)}></input>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
     </>
   );
+};
+
+App.defaultProps = {
+  name: "sample",
+  price: 1000,
 };
 
 export default App;
